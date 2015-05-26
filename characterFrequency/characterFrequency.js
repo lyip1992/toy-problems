6433edee1f0b465c1ee5b4b1c9fdd38198f7d8e2
Leon Yip <lyip1992@gmail.com>
2015-05-26 09:55:30 -0700

    attempted characterfrequency

--- characterFrequency/characterFrequency.js
+++ characterFrequency/characterFrequency.js
@@ -37,5 +37,55 @@
 
 
 var characterFrequency = function(string) {
+  var dictionary = {};
+  var secondDictionary = {};
+  var numbers = [];
+  var letters = [];
+  var result = [];
+
+  for( var i = 0; i < string.length; i++ ) {
+    if( !dictionary[string[i]] ) dictionary[string[i]] = 1;
+    else dictionary[string[i]] += 1;
+  }
+  
+  for( var key in dictionary ) {
+    numbers.push(dictionary[key]);
+  }
+
+  numbers.sort(function(a, b) { return b - a; });
+
+  // sort by frequency
+  for( var i = 0; i < numbers.length; i++ ) {
+  	for( var key in dictionary ) {
+  	  if( letters.indexOf(key) === -1 && numbers[i] === dictionary[key])
+  	  	letters.push(key);
+  	}
+  }
+  
+  for( var i = 0; i < letters.length; i++ ) {
+  	if( letters[i] > letters[i + 1] && numbers[i] === numbers[i + 1]) {
+  	  var temp = letters[i];
+  	  letters[i] = letters[i + 1];
+  	  letters[i + 1] = temp;
+  	}
+  }
+  // sort by character
+  for( var i = 0; i < letters.length; i++ ) {
+  	result.push([letters[i], numbers[i]]);
+  }
+
   return result;
 };
+
+console.log(characterFrequency('mmmaaaiiibbb'));
+
+
+
+
+
+
+
+
+
+
+
