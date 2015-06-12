efc0e1d5a43d97c8671eee5b6bee5a02917cfe50
Leon Yip <lyip1992@gmail.com>
2015-06-12 09:56:13 -0700

    first attempt at longestPalindrome - to be redone

--- longestPalindrome/longestPalindrome.js
+++ longestPalindrome/longestPalindrome.js
@@ -6,5 +6,35 @@
 * whitespace on each side of dad).
 */
 
-var longestPalindrome = function (string) {
+var longestPalindrome = function(string) {
+
+	var maxPalinLength = 0;
+	var longestPalindrome = null;
+	var length = string.length;
+
+	// check all possible sub strings
+	for (var i = 0; i < length; i++) {
+		for (var j = i + 1; j < length; j++) {
+			var len = j - i;
+			var curr = string.substring(i, j + 1);
+			if (isPalindrome(curr)) {
+				if (len > maxPalinLength) {
+					longestPalindrome = curr;
+					maxPalinLength = len;
+				}
+			}
+		}
+	}
+
+	return longestPalindrome;
+};
+
+var isPalindrome = function(string) {
+
+	for (var i = 0; i < string.length - 1; i++) {
+		if (string.charAt(i) != string.charAt(string.length - 1 - i)) {
+			return false;
+		}
+	}
+	return true;
 };
