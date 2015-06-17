2ade123102fe7062920f0f6ef9c8974212023a3e
Leon Yip <lyip1992@gmail.com>
2015-06-17 09:56:33 -0700

    first attempt at spiralTraversal

--- spiralTraversal/spiralTraversal.js
+++ spiralTraversal/spiralTraversal.js
@@ -14,7 +14,20 @@
     returns [1, 2, 3, 6, 9, 8, 7, 4, 5]
  */
 
-var spiralTraversal = function(matrix){
+var spiralTraversal = function(matrix) {
+  var result = [];
 
-  // TODO: Implement me!
+  for( var i = 0; i < iterations.length * 2; i++ ) {
+    
+
+  }
+
+
 };
+
+
+// 1 x 1 gives 1 || 1
+// 2 x 2 gives 3 || 4
+// 3 x 3 gives 5 || 9
+// 4 x 4 gives 7 || 16
+// 5 x 5 gives 9 || 25
