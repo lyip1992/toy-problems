dad4ecd412aef4c0275188bdebced8f5a00deda6
Leon Yip <lyip1992@gmail.com>
2015-07-03 09:13:53 -0700

    slight refactor

--- queueStack/queueStack.js
+++ queueStack/queueStack.js
@@ -7,20 +7,20 @@
   * Stack Class
   */
 var Stack = function() {
-  this._storage = {};
+  this._storage = [];
   this._size = 0;
 };
 
 Stack.prototype.push = function(item){ // add an item to the top of the stack
-  this._storage[this._size] = item;
-  this._size++;
+  this._storage[this._size++] = value;
 };
 
 Stack.prototype.pop = function(){ // remove an item from the top of the stack
-  var temp = this._storage[this._size - 1];
-  delete this._storage[this._size - 1];
-  this._size--;
-  return temp;
+  if( this._size ){
+    var temp = this._storage[--this._size];
+    delete this._storage[this._size];
+    return temp;
+  }
 };
 
 Stack.prototype.size = function(){ // return the number of items in the stack
@@ -41,14 +41,12 @@ Queue.prototype.enqueue = function(item){ // called to add an item to the `queue
 };
 
 Queue.prototype.dequeue = function(){ // called to remove an item from the `queue`
-  if( !this._outbox.size() && this._inbox.size() ){
-    while( this._inbox.size() ){  // while inbox size is greater than 0
+  if( this._outbox.size() === 0 ){
+    while( this._inbox.size() !== 0 ){  // while inbox size is greater than 0
       this._outbox.push(this._inbox.pop());  // pop item off inbox and put it into the outbox
     }
   }
-
-  if( this._outbox.size() ){ return this._outbox.pop(); }  // if there's something in our outbox, pop it off
-  return null;  // else return null
+  return this._outbox.pop();
 };
 
 Queue.prototype.size = function(){ // should return the number of items in the queue
