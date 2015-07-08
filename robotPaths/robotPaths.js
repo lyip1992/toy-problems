564983d4535337a65c9fdc8e519625e59dcf106f
Leon Yip <lyip1992@gmail.com>
2015-07-07 20:56:26 -0700

    second attempt

--- robotPaths/robotPaths.js
+++ robotPaths/robotPaths.js
@@ -28,25 +28,22 @@ var makeBoard = function(n) {
   return board;
 };
 
-var robotPaths = function(n, board, i, j, solutions) {
-  solutions = solutions || 0;
-  board = board || makeBoard(n);
-  i = i || 0;
-  j = j || 0;
+var robotPaths = function(n, board, i, j){
+  if( !board ){
+    board = makeBoard(n);
+    i = 0;
+    j = 0;
+  }
 
-  if( i < 0 || j < 0 || i > n || j > n ){ return 0; }
+  if( !(i >= 0 && i < n && j >= 0 && j < n) || board.hasBeenVisited(i, j) ){ return 0; }
+  if( i === n - 1 &&  j === n - 1 ){ return 1;}
 
-  if( !board.hasBeenVisited(i, j) ) { board.togglePiece(i, j); } // if the current spot hasn't been visited, toggle piece
-  if( board.hasBeenVisited(n - 1, n - 1) ) return 1; // if we are on the bottom right corner, increment solution
+  board.togglePiece(i, j);
+  var result = robotPaths(n, board, i, j + 1) +
+               robotPaths(n, board, i, j - 1) +
+               robotPaths(n, board, i + 1, j) +
+               robotPaths(n, board, i - 1, j);
 
-  // go up
-  solutions += robotPaths(n, board, i - 1, j);
-  // go down
-  solutions += robotPaths(n, board, i + 1, j);
-  // go left
-  solutions += robotPaths(n, board, i, j - 1);
-  // go right
-  solutions += robotPaths(n, board, i, j + 1);
-
-  return solutions;
+  board.togglePiece(i, j);
+  return result;
 };
