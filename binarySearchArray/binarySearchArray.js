5de05a63a3a64a2dfc626a2bac85e9108e1f3f96
Leon Yip <lyip1992@gmail.com>
2015-07-25 09:37:09 -0700

    first attempt

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -8,6 +8,25 @@
  * console.log(index); // [3]
  */
 
-var binarySearch = function (array, target) {
-};
+var binarySearch = function(array, target, current){
 
+  current = current || 0;
+  var midpoint = Math.floor(array.length / 2);
+
+  if( array.length === 1 ){
+    if( array[0] === target ){
+      return current;
+    } else {
+      return null;
+    }
+  }
+
+  if( array[midpoint] === target ){
+    return current + midpoint;
+  } else if( target < array[midpoint] ){
+    return binarySearch(array.slice(0, midpoint), target, current);
+  } else {
+    return binarySearch(array.slice(midpoint), target, current + midpoint);
+  }
+
+};
