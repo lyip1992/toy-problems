d39286f2053a8cc777ccfe0e8e3a3e9751b7e75f
Leon Yip <lyip1992@gmail.com>
2015-07-28 09:57:03 -0700

    first attempt

--- rangeClass/rangeClass.js
+++ rangeClass/rangeClass.js
@@ -40,16 +40,105 @@
 
 
 var Range = function(start, end, step) {
+  if( start !== undefined ){
+    this.start = start;
+  } else {
+    this.start = null;
+  }
+  this.end = end || start;
+
+  this.step = step || 1;
 };
 
-Range.prototype.size = function () {
+Range.prototype.size = function(){
+  var start = this.start;
+  var end = this.end;
+  var step = this.step;
+  var counter = 0;
+
+  // if the start value doesn't exist
+  if( start === null ){ return null; }
+
+  // if the start value exists but the end value doesn't exist
+  if( start !== null && end === start ){ return 1; }
+
+  if( start !== null && end !== start ){ // if the start and end value exist
+    if( start > end ){ // if the start value is greater than the end value
+      while( start >= end ){
+        start -= step;
+        counter++
+      }
+      return counter;
+    } else if( start < end ){ // if the start value is less than the end value
+      while( start <= end ){
+        end -= step;
+        counter++;
+      }
+      return counter;
+    }
+  }
 };
 
-Range.prototype.each = function (callback) {
+Range.prototype.each = function(callback){
+  var start = this.start;
+  var end = this.end;
+  var step = this.step;
+
+  if( start === null ){ return null; }
+
+  if( start !== null && end === start ){ return callback(start); }
+
+  if( start !== null && end !== start ){
+    if( start > end ){
+      while( start >= end ){
+        callback(start);
+        start -= step;
+      }
+      return;
+    } else if( start < end ){
+      while( start <= end ){
+        callback(end);
+        end -= step;
+      }
+      return;
+    }
+  }
 };
 
-Range.prototype.includes = function (val) {
+Range.prototype.includes = function(val){
+  var start = this.start;
+  var end = this.end;
+  var step = this.step;
+  var tracker = false;
+
+  if( start === null ){ return tracker; }
+
+  if( start !== null && end === start ){ return start === val; }
+
+  if( start !== null && end !== start ){
+    if( start > end ){
+      while( start >= end ){
+        if( start === val ){ tracker = true; }
+        start -= step;
+      }
+      return tracker;
+    } else if( start < end ){
+      while( start <= end ){
+        if( end === val ){ tracker = true; }
+        end -= step;
+      }
+      return tracker;
+    }
+  }
 };
 
-var range = new Range(1);
 
+var evenNumbers = new Range(2,8,2); // A range with the even numbers 2, 4, 6, and 8.
+evenNumbers.each(function(val){
+  console.log(val+"!");
+});
+console.log("Who do we appreciate!?");
+
+evenNumbers.size();
+evenNumbers.includes(2);
+evenNumbers.includes(3);
