428f1f172e4203faf170b9afcb45c9dcba291727
Leon Yip <lyip1992@gmail.com>
2015-08-13 11:17:53 -0700

    second attempt

--- fractionConverter/fractionConverter.js
+++ fractionConverter/fractionConverter.js
@@ -2,45 +2,41 @@
  * Write a function that takes a number as its argument and
  * returns a string that represents that number's simplified fraction.
  *
- * Example: toFraction(0.5) === '5/10'
+ * Example: toFraction(0.5) === '1/2'
  *
  * Whole numbers and mixed fractions should be returned as irregular fractions
  *
- * Example: toFraction(3.0) === '30/10'
+ * Example: toFraction(3.0) === '3/1'
  *
- * Example: toFraction(2.5) === '25/10'
- *
- * Example: toFraction(2.2) === '22/10'
- *
- * Example: toFraction(2.3) === '23/10'
+ * Example: toFraction(2.5) === '5/2'
  *
  */
 
 var toFraction = function(number) {
-
-  var whole = Math.floor(number);
-  var decimal = number - whole;
-  var decimalMod = decimal;
-  var tens = 0;
-
-  // how many times to i have to multiply my decimal by 10 before i get to  >= 1?
-  if( decimal !== 0 ){
-    tens++;
+  var isNegative = number < 0;
+  var string = number.toString();
+  var split = string.split('.');
+  if( isNegative ){ number = number * -1; }
+  var denominator;
+  if( split[1] ){
+    denominator = Math.pow(10, split[1].length);
   } else {
-    while( decimalMod < 1 ){
-      tens++;
-      decimalMod *= 10;
+    denominator = 1;
+  }
+  var numerator = Math.round(number * denominator);
+  return (isNegative ? '-' : '') + reduce(numerator, denominator).join('/');
+};
+
+var reduce = function(numerator, denominator){
+  var lowNum = Math.min(numerator, denominator);
+  for( var i = lowNum; i > 0; i-- ){
+    if( i > numerator || i > denominator ){
+      return [numerator, denominator];
+    }
+    if ( numerator % i === 0 && denominator % i === 0 ){
+      numerator /= i;
+      denominator /= i;
     }
   }
-
-  // start cacheing our numbers and turn them into strings
-  var numerator = number * tens * 10;
-  var denominator = tens * 10;
-
-  // now how do we simplify our number?
-  // we could keep dividing by 5 and two until we get our simplified fraction
-
-
-  return numerator + '/' + denominator;
-
+  return [numerator, denominator];
 };
