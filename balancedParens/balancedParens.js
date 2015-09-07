5770525789190add1f47c485dda32367b389a0b7
Leon Yip <lyip1992@gmail.com>
2015-09-06 19:18:46 -0700

    (feat) remove console logs

--- balancedParens/balancedParens.js
+++ balancedParens/balancedParens.js
@@ -52,15 +52,3 @@ var balancedParens = function(input){
 
   return stack.length === 0;
 };
-
-console.log(balancedParens('('));  // false
-console.log(balancedParens('()')); // true
-console.log(balancedParens(')('));  // false
-console.log(balancedParens('(())'));  // true
-
-console.log(balancedParens('[](){}')); // true
-console.log(balancedParens('[({})]'));   // true
-console.log(balancedParens('[(]{)}')); // false
-
-console.log(balancedParens(' var wow  = { yo: thisIsAwesome() }')); // true
-console.log(balancedParens(' var hubble = function() { telescopes.awesome();')); // false
