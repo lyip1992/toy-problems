52b0c4304340ec60cf7b484fd2ae5e148e6ffded
Leon Yip <lyip1992@gmail.com>
2015-10-25 16:44:04 -0700

    (feat) second attempt

--- primeTester/primeTester.js
+++ primeTester/primeTester.js
@@ -4,55 +4,51 @@
  * a prime number, false if it's not.
  */
 
-var primeTester = function(n) {
-  if(typeof n !== 'number' || n < 1 || n % 1 !== 0){
-    // n isn't a number or n is less than 1 or n is not an integer
-    return false;
-  }
-  // the brute force method would be to test for everything under between itself and one
-  // this would be one massive for loop
-  // at the end we'll have some thing that stars off true and returns false as soon as the modulus gives
-  // something that isn't zero
+ var primeTester = function(n){
+   if( typeof n !== 'number' || n < 1 || n % 1 !== 0 ){ // n isn't a number or n is less than 1 or n is not an integer
+     return false;
+   }
 
-  for( var i = n - 1 ; i > 1; i-- )
-  	if( !(n % i) ) return false;
+   var upperLimit = Math.sqrt(Math.abs(n));
 
-  return true;
-};
+   if( n === 1 ){ // `1` is not prime
+     return false;
+   }
 
-/* Extra credit: Write a function that generates a list of all prime numbers
- * in a user-specified range (inclusive). If you're not quite sure where to start,
- * check out the Sieve of Eratosthenes on Wikipedia. (And if you're feeling
- * saucy, check out the Sieve of Atkin.)
- */
+   for( var i = 2; i <= upperLimit; i++ ){
+     if( n % i === 0 ){
+       return false;
+     }
+   }
 
-var primeSieve = function (start, end) {
-  
-  var primes = []; 
+   return true;
+ };
 
-  for( var i = start; i <= end; i++ ) {
-  	if( primeTester(i) ) primes.push(i); 
-  }
+ /* Extra credit: Write a function that generates a list of all prime numbers
+  * in a user-specified range (inclusive). If you're not quite sure where to start,
+  * check out the Sieve of Eratosthenes on Wikipedia. (And if you're feeling
+  * saucy, check out the Sieve of Atkin.)
+  */
 
-  return primes;
+ var primeSieve = function(start, end){
+   var current = 2;
+   var primes = range(0, end + 1);
 
-  // var result = [];
-  // var storage = {};
+   while( current < end ){ // While we haven't found all the primes.
+     for( var i = current + current; i <= end; i += current ){ // mark all multiples of current as not prime
+       primes[i] = null; // null means not prime, a number means prime
+     }
+     do { // find the next current
+       current += 1; // Advance current at least once
+     } while (!primes[current] && current <= end); // Then continue to advance it until we hit a prime number or we are out of range.
+   }
+   return primes.slice(2).filter(function(val){ return val && val >= start; }); // Filter out all values that aren't prime and aren't in our range.
+ };
 
-  // for( var i = 2; i <= end; i++) 
-  // 	storage[i] = 0;
-
-  // for( var i = 2; i <= end; i++) {
-  // 	for( var j = i; j <= end; j * j) {
-  // 	  storage[j] += 1
-  // 	}
-  // }  
-  
-  // for( var key in storage ) {
-  // 	if( storage[key] === 1 key >== start) result.push(key);
-  // }
-
-  // return result;
-};
-
-
+ var range = function(start, end){
+   var result = [];
+   for( var i = start; i < end; i++ ){
+     result.push(i);
+   }
+   return result;
+ };
